bca7315eac4d6c633d75888de4ea7f13e42e5001
Nadim Ritter <nadim.ritter@ch.ibm.com>
2018-12-20 15:40:06 +0100

    added addiotion and subthraction

---
+++ src/main/java/com/mycompany/consolecalculator/App.java
@@ -0,0 +1,26 @@
+package com.mycompany.consolecalculator;
+
+public class App {
+    
+    public static void main(String[] args) {
+        
+        Calculator calculator = new Calculator();
+        int firstNumber = 0;
+        int secondNumber = 0;
+        
+        System.out.println("Console Calculator");
+        System.out.println("==================");
+        
+        firstNumber = 100;
+        secondNumber = 200;
+        System.out.println("Summe: " + firstNumber + " + " + secondNumber + " = " + calculator.addition(firstNumber, secondNumber));
+        System.out.println("Differenz: " + firstNumber + " - " + secondNumber + " = " + calculator.subtraction(firstNumber, secondNumber));
+        
+        
+    }
+    
+    
+    
+    
+
+}
